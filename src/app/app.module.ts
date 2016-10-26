e19cc6de67fb81159bc17961f105435a31e8a240
Jordan Pena <jordan.pena1@gmail.com>
2016-10-26 01:38:47 -0200

    criando paginas institucionais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpModule } from '@angular/http';
 import { PolymerElement } from '@vaadin/angular2-polymer';
 
 import { AppComponent } from './app.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SobreComponent,
+    FeedComponent,
+    PolymerElement('paper-card'),
     PolymerElement('app-header'),
+    PolymerElement('paper-button'),
     PolymerElement('app-header-layout'),
     PolymerElement('paper-icon-button'),
     PolymerElement('paper-icon-item'),
@@ -17,6 +23,8 @@ import { AppComponent } from './app.component';
     PolymerElement('app-drawer'),
     PolymerElement('paper-item'),
     PolymerElement('paper-menu'),
+    PolymerElement('app-box'),
+     PolymerElement('iron-pages'),
     PolymerElement('app-toolbar')
   ],
   imports: [
---
+++ src/app/sobre/sobre.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SobreComponent } from './sobre.component';
+
+describe('Component: Sobre', () => {
+  it('should create an instance', () => {
+    let component = new SobreComponent();
+    expect(component).toBeTruthy();
+  });
+});
